46874e63b94fbfba176adc32b9b8aa82cf3177b6
Eduard Valeyev <eduard@valeyev.net>
2015-04-09 20:39:40 -0400

    Remove changes useful for debugging in preparation for pull request

--- tiledarray/iTEBD_imag.cpp
+++ tiledarray/iTEBD_imag.cpp
@@ -95,9 +95,6 @@ int main (int argc, char* argv[])
 {
   madness::World& world = madness::initialize(argc,argv);
 
-  int i;
-  if (world.rank() == 0) std::cin >> i;
-
   // iTEBD (J, Jz, Hz, dt, "# step",
   //        "# initial quanta",
   //        "# initial states for each quantum",
